c1c6bbfcb38ebd6209dee45ed6347f0cc092773f
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-01 11:54:27 +0000

    safe_cracker part 2

--- 2025/day-01/safe_cracker.py
+++ 2025/day-01/safe_cracker.py
@@ -1,7 +1,8 @@
 # Day 1 code for AoC
 # Created 2025-12-01 by Joseph Hall
 
-def main(input_path="2025/day-01/input-01.txt"):
+
+def part_1(input_path="2025/day-01/input-01.txt"):
 
     with open(input_path, "r") as f:
         pass_code = f.readlines()
@@ -11,10 +12,8 @@ def main(input_path="2025/day-01/input-01.txt"):
     for line in pass_code:
         # Extract number of clicks
         n_clicks = int(line[1:-1])
-        print(line[0])
         # Pass direction
         if line[0].upper() == "L":
-            print("left")
             tot -= n_clicks
         else:
             tot += n_clicks
@@ -26,6 +25,34 @@ def main(input_path="2025/day-01/input-01.txt"):
     
     print(pwd)
     return
+
+
+def part_2(input_path="2025/day-01/input-01.txt"):
+
+    with open(input_path, "r") as f:
+        pass_code = f.readlines()
+
+    tot = 50
+    pwd = 0
+    for line in pass_code:
+        # Extract number of clicks
+        n_clicks = int(line[1:-1])
+
+        # This is a terrible solution, but yeet
+        positive = line[0].upper() == "R"
+        for click in range(n_clicks):
+            tot = tot + 1 if positive else tot - 1
+            # Check to see if at 0
+            if (tot % 100) == 0:
+                pwd += 1
+    
+    print("part 2 password:", pwd)
+    return
+
+
+def main(input_path="2025/day-01/input-01.txt"):
+    part_1(input_path)
+    part_2(input_path)
 
 
 if __name__ == "__main__":
