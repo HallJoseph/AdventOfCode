844f1a6e4cd615e70a9aa7cb14b27b6146afdf5b
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-02 10:47:24 +0000

    Initial commit for 2025/day-02

---
+++ 2025/day-02/bar_codes.py
@@ -0,0 +1,19 @@
+# Day 2 code for AoC
+# Created 2025-12-01 by Joseph Hall
+
+def part_1():
+    return
+
+
+def part_2():
+    return
+
+
+def main(input_path="2025/day-02/input-02.txt"):
+    with open(input_path, "r") as f:
+        input_data = f.read()
+    return
+
+
+if __name__ == "__main__":
+    main()
