645372a7577d0c073445dbb97dd52194883ade1a
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-03 12:42:43 +0000

    Initial commit of day 3 code

---
+++ 2025/day-03/batteries.py
@@ -0,0 +1,17 @@
+# Code for day 3 of AoC 2025
+# Created: 2025-12-03 by Joseph Hall
+
+def part_1(input_data):
+    return
+
+
+def part_2(input_data):
+    return
+
+
+def main(input_path="2025/day-03/input-03.txt"):
+    return
+
+
+if __name__ == "__main__":
+    main()
