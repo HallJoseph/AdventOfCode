987efbd15f1e1fb81eedc47e3d05b93616f4e216
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-03 12:50:56 +0000

    Part 1 solved

--- 2025/day-03/batteries.py
+++ 2025/day-03/batteries.py
@@ -2,6 +2,21 @@
 # Created: 2025-12-03 by Joseph Hall
 
 def part_1(input_data):
+    total_joltage = 0
+
+    # Find the index of the maximum in the input row up to -1th
+    for row in input_data:
+        row_max = max(row[:-1])
+        for vind, val in enumerate(row):
+            if val == row_max:
+                # Check for second digit
+                second_digit = max(row[vind+1:])
+                
+                # Combine digits and add to total joltage
+                total_joltage += int(f"{row_max}{second_digit}")
+                break
+    
+    print("Part 1 soln:", total_joltage)
     return
 
 
@@ -15,7 +30,7 @@ def main(input_path="2025/day-03/input-03.txt"):
         input_data = f.readlines()
 
     input_data = [[int(x) for x in y[:-1]] for y in input_data]
-    
+    part_1(input_data)
     return
 
 
