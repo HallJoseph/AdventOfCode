6608191fea1ea3be0afd4d3fc4d6f97a000639bc
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-04 14:02:56 +0000

    committing standard code structure outline

---
+++ 2025/code_structure.py
@@ -0,0 +1,18 @@
+# Basic code structure for AoC 2025
+# Created: 2025-12-04, by Joseph Hall
+
+
+def part_1(input_data):
+    return
+
+
+def part_2(input_data):
+    return
+
+
+def main(input_path=""):
+    return
+
+
+if __name__ == "__main__":
+    main()
