6a30a9926cc78653b1309f8f2f8e9c88c6a47738
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-06 17:09:24 +0000

    initial commit for day 6 problem

---
+++ 2025/day-06/maths-homework.py
@@ -0,0 +1,18 @@
+# Code for AoC 2025 day 6
+# Created: 2025-12-06, by Joseph Hall
+
+
+def part_1(input_data):
+    return
+
+
+def part_2(input_data):
+    return
+
+
+def main(input_path=""):
+    return
+
+
+if __name__ == "__main__":
+    main()
