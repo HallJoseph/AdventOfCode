51f2bab417622879006e6714656beaf5d8280576
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-07 15:34:55 +0000

    Visualising Day 7 Part 2 solution

--- 2025/day-07/tachyon-splitter.py
+++ 2025/day-07/tachyon-splitter.py
@@ -2,6 +2,38 @@
 # Created: 2025-12-07, by Joseph Hall
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import LogNorm
+
+def animate(frames):
+    fig, ax = plt.subplots()
+    div = make_axes_locatable(ax)
+    cax = div.append_axes('right', '5%', '5%')
+    
+    init_frame = frames[0]
+    im = ax.imshow(init_frame, origin='lower', norm=LogNorm(), cmap="inferno") # Here make an AxesImage rather than contour
+    cb = fig.colorbar(im, cax=cax)
+    tx = ax.set_title('Level 0')
+
+    def animate_loc(i):
+        arr = frames[i]
+        vmax     = np.max(arr)
+        vmin     = np.min(arr)
+        im.set_data(arr)
+        im.set_clim(vmin, vmax)
+        tx.set_text(f'Level {i}')
+
+    ani = animation.FuncAnimation(fig, animate_loc, frames=len(frames), interval=100,
+                                    repeat_delay=5000)
+    # ani = animation.ArtistAnimation(fig, ims)
+
+    writer = animation.FFMpegWriter(
+        fps=15, metadata=dict(artist='Me'), bitrate=1800)
+    
+    ani.save("2025/day-07/part-2-visual.gif", writer=writer)
+    plt.show()
 
 
 def part_1(input_data):
@@ -44,7 +76,7 @@ def part_1(input_data):
 def part_2(input_data):
     # Create a matrix to store number of timelines each splitter produces
     cache_matrix = np.ones_like(input_data, dtype=int)
-
+    vis_arrs = [cache_matrix.copy()]
     # Work from bottom up
     input_data_flip = input_data[::-1].copy()
     for rind, row in enumerate(input_data_flip):
@@ -63,9 +95,11 @@ def part_2(input_data):
         new_right[new_right_inds] += 1
         
         cache_matrix[rind:, split_inds] = cache_matrix[rind, new_left_inds] + cache_matrix[rind, new_right_inds]
-    
+        vis_arrs.append(cache_matrix.copy())
+
     # Print solution as last value at position corresponding to S in last row of cache matrix
     print("Part 2 solution:", cache_matrix[-1][input_data[0]=="S"])
+    animate(vis_arrs)
     return
 
 
