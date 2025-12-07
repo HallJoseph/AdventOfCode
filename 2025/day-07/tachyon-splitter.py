31da386f4960e5d91fca8d3d10840cc7210b0918
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-07 13:56:11 +0000

    day 7 initial commit

---
+++ 2025/day-07/tachyon-splitter.py
@@ -0,0 +1,23 @@
+# Code for AoC 2025 Day 7
+# Created: 2025-12-07, by Joseph Hall
+
+import numpy as np
+
+def part_1(input_data):
+    return
+
+
+def part_2(input_data):
+    return
+
+
+def main(input_path="2025/day-07/input-07.txt"):
+    # Load the data
+    input_data = np.loadtxt(input_path, dtype=str)
+    input_arr = np.array([list(x) for x in input_data])
+    print(input_arr)
+    return
+
+
+if __name__ == "__main__":
+    main()
