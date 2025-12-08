74a101ab4b55bdd278f3b88305d5a32258966e5e
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-08 11:23:28 +0000

    Day 8 initial commit

---
+++ 2025/day-08/cable_networks.py
@@ -0,0 +1,20 @@
+# Code for AoC 2025 day 8
+# Created: 2025-12-08, by Joseph Hall
+
+import numpy as np
+
+def part_1(input_data):
+    return
+
+
+def part_2(input_data):
+    return
+
+
+def main(input_path="2025/day-08/input-08.txt"):
+    input_data = np.loadtxt(input_path, delimiter=",")
+    return
+
+
+if __name__ == "__main__":
+    main()
