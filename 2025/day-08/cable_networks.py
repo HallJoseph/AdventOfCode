4206defd62b0387d1158f650f702eabd24102bde
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-08 12:04:38 +0000

    Part 1 solved

--- 2025/day-08/cable_networks.py
+++ 2025/day-08/cable_networks.py
@@ -2,8 +2,54 @@
 # Created: 2025-12-08, by Joseph Hall
 
 import numpy as np
+import pandas as pd
+import tqdm
+import networkx as nx
 
 def part_1(input_data):
+    # Find distances between all pairs of nodes
+    input_size = len(input_data)
+    dists = []
+    starts = []
+    ends = []
+    for nind, node in enumerate(tqdm.tqdm(input_data)):
+        node_dists = np.sqrt(np.sum((input_data[nind+1:] - node)**2, axis=1)).tolist()
+        node_starts = [nind] * (input_size-(nind+1))
+        node_ends = [x for x in range(nind+1, input_size)]
+        
+        dists += node_dists
+        starts += node_starts
+        ends += node_ends
+    
+    # Convert to dataframe
+    dists_df = pd.DataFrame.from_records(
+        {
+            "starts": starts,
+            "ends": ends,
+            "dists": dists
+        }
+    )
+    
+    # Find k shortest dists
+    k = 1000
+    shortest_dists = dists_df.sort_values(by="dists")[:k]
+    shortest_dists = shortest_dists[["starts", "ends", "dists"]]
+    edges_arr = shortest_dists.values
+    print(edges_arr)
+    
+    # Use shortest dists to build graph
+    G = nx.Graph()
+    G.add_nodes_from([x for x in range(input_size)])
+    G.add_weighted_edges_from(edges_arr)
+    
+    # Find the sizes of each of the networks
+    network_sizes = [len(x) for x in nx.connected_components(G)]
+    network_sizes.sort(reverse=True)
+    print(network_sizes)
+
+    # Solution is the product of the first n values in network sizes
+    n=3
+    print("Part 1 sol:", np.prod(network_sizes[:n]))
     return
 
 
@@ -13,6 +59,7 @@ def part_2(input_data):
 
 def main(input_path="2025/day-08/input-08.txt"):
     input_data = np.loadtxt(input_path, delimiter=",")
+    part_1(input_data)
     return
 
 
