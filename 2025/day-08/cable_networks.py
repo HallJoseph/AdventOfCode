c8aced1cc0c0d1912a467e36523f7ceb3e089f55
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-08 12:11:31 +0000

    Day 2 solved

--- 2025/day-08/cable_networks.py
+++ 2025/day-08/cable_networks.py
@@ -28,12 +28,13 @@ def part_1(input_data):
             "ends": ends,
             "dists": dists
         }
-    )
+    ).sort_values(by="dists")
+    dists_df = dists_df[["starts", "ends", "dists"]]
     
     # Find k shortest dists
     k = 1000
-    shortest_dists = dists_df.sort_values(by="dists")[:k]
-    shortest_dists = shortest_dists[["starts", "ends", "dists"]]
+    shortest_dists = dists_df[:k]
+    shortest_dists = shortest_dists
     edges_arr = shortest_dists.values
     print(edges_arr)
     
@@ -50,6 +51,18 @@ def part_1(input_data):
     # Solution is the product of the first n values in network sizes
     n=3
     print("Part 1 sol:", np.prod(network_sizes[:n]))
+
+    # Part 2 solution -- coding here rather than separate function
+    other_edges = dists_df[k:].values
+    for edge in other_edges:
+        G.add_weighted_edges_from([edge])
+        if len(list(nx.connected_components(G))) == 1:
+            final_x1 = input_data[int(edge[0]), 0]
+            final_x2 = input_data[int(edge[1]), 0]
+
+            print("Part 2 sol:", final_x1 * final_x2)
+            break
+
     return
 
 
