f9d0fbee8375c7d400f0f277daae1900c4f1901c
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-10 17:33:49 +0000

    Day 10 part 1 solved

--- 2025/day-10/factory-batteries.py
+++ 2025/day-10/factory-batteries.py
@@ -1,8 +1,50 @@
 # AoC day 10 script
 # Created: 2025-12-10, by Joseph Hall
 
+import numpy as np
+import itertools
+import tqdm
 
 def part_1(input_data):
+    req_presses = 0
+
+    for row in tqdm.tqdm(input_data):
+        # Parse row info
+        split_lights = row.split("]")
+        lights = split_lights[0][1:]
+        inputs_jolts = split_lights[1].split("{")
+        inputs = inputs_jolts[0].strip()[1:-1].split(") (")
+
+        # Turn lights into boolean array
+        required_lights = np.array([True if x == "#" else False for x in lights], dtype=bool)
+        start_lights = np.zeros_like(required_lights, dtype=bool)
+
+        # Turn inputs into a list of tuples of ints
+        input_inds = [np.array(tuple(y.replace(',', '')), dtype=int) for y in inputs]
+
+        # Start looping over combinations of n inputs, increasing n each time till valid input found
+        n = 1
+        solved = False
+
+        while not solved:
+            button_perms = list(itertools.permutations(input_inds, n))
+
+            for perm in button_perms:
+                # Reset the lights
+                current_lights = start_lights.copy()
+                for press in perm:
+                    # Switch the input at this index
+                    current_lights[press] = ~current_lights[press]
+
+                # After pressing all the buttons check if we've matched the desired lights
+                if current_lights.tolist() == required_lights.tolist():
+                    solved = True
+                    req_presses += n
+                    break
+
+            n += 1
+
+    print("Part 1 sol:", req_presses)
     return
 
 
