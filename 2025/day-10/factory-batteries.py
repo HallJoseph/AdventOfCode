6da22860af597b52866c89996998bdb37bcff12a
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-10 17:07:20 +0000

    day 10 initial commit

---
+++ 2025/day-10/factory-batteries.py
@@ -0,0 +1,21 @@
+# AoC day 10 script
+# Created: 2025-12-10, by Joseph Hall
+
+
+def part_1(input_data):
+    return
+
+
+def part_2(input_data):
+    return
+
+
+def main(input_path="2025/day-10/input-10.txt"):
+    with open(input_path, "r") as f:
+        input_data = f.readlines()
+    part_1(input_data)
+    return
+
+
+if __name__ == "__main__":
+    main()
