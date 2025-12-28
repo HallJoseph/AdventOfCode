a9f0cccd8789edb3df56395bcd19d93a4b813660
Joseph Hall <joseph.hall@bristol.ac.uk>
2025-12-28 13:24:47 +0000

    Recoded day-11 part 2 to use cached searching, vastly improving time to complete

--- 2025/day-11/reactor-server.py
+++ 2025/day-11/reactor-server.py
@@ -54,7 +54,49 @@ def part_1(input_data):
     return
 
 
+def path_count(current, end, conn_dict, cache={}):
+    # Function with caching for trying to faster find number of paths between two nodes
+    #   Adapted from @DariusMichienzi's day 11 code in julia
+
+    # Check if at the end of the chain, and return 1 (i.e. 1 path to get there foun)
+    if current == end:
+        return 1
+    
+    # Check if the number of paths between current and end needs to be found
+    if (current, end) not in cache.keys():
+        # Find the number of available paths with the holy power of recursion
+        paths = 0
+
+        # Split on if looking for the output or looking for another node (to allow escape
+        #   if reaching end of graph without finding a valid path)
+        if end == "out":
+            paths = sum([path_count(conn, end, conn_dict, cache) for conn in conn_dict[current]])
+
+        else:
+            # Iterate over connections and reject if output: this allows 0 paths to be found
+            for conn in conn_dict[current]:
+                if conn != "out":
+                    paths += path_count(conn, end, conn_dict, cache)
+
+        # Update the connections dictionary with the number of paths we've found
+        cache[current, end] = paths
+
+    return cache[current, end]
+
+
 def part_2(input_data):
+    # Redoing part 2 here, but with my own (read: adapted from Darius' code) cached search
+    #   function which should hopefully be a bit faster than the one from networkx
+
+    # Parse the input data into a dictionary
+    conn_dict = {}
+    for node_conn in input_data:
+        node_split = node_conn.split(":")
+        conn_dict[node_split[0]] = node_split[1].strip().split(" ")
+
+    # Find the paths by multiplying svr --> ftt, fft --> dac, and dac --> out (Might need MPing)
+    paths = path_count("svr", "fft", conn_dict) * path_count("fft", "dac", conn_dict) * path_count("dac", "out", conn_dict)
+    print("Part 2 sol:", paths)
     return
 
 
@@ -62,7 +104,8 @@ def main(input_path="2025/day-11/input-11.txt"):
     with open(input_path, "r") as f:
         input_data = f.readlines()
     input_data = [x[:-1] for x in input_data]
-    part_1(input_data)
+
+    part_2(input_data)
     return
 
 
